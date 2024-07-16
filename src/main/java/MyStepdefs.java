8ac7c9a9644adea4b2b011e18f50a9eeb675a1ad
Student <csabakocsany@gmail.com>
2024-07-16 14:15:44 +0200

    4

---
+++ src/main/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I am on the homepage")
+    public void iAmOnTheHomepage() {
+    }
+
+    @When("i search a {string} word in search bar")
+    public void iSearchAWordInSearchBar(String product) {
+    }
+
+    @Then("i should see the {string} product")
+    public void iShouldSeeTheProduct(String product) {
+    }
+}
