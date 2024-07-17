8fdd79d87fb5a34d24e7ba2b1c1323f4d2a9d120
Student <csabakocsany@gmail.com>
2024-07-17 15:19:02 +0200

    4

--- src/main/java/hu/masterfield/steps/TescoSteps.java
+++ src/main/java/hu/masterfield/steps/TescoSteps.java
@@ -1,19 +1,32 @@
 package hu.masterfield.steps;
 
 import hu.masterfield.pages.HomePage;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TescoSteps {
     @BeforeAll
     public static void setup() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments();
-        
 
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.manage().window().setSize(new Dimenson(800, 800));
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        driver.quit();
     }
 
     @Given("I am on the homepage")
